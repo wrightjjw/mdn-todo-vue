b4ea079c7aee2d4cf440eafd672f0bab0f9e9a1a
Josh Wright <wright.jjw@gmail.com>
2021-02-22 22:47:03 -0500

    finished through "creating our first vue component"

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>To-Do List</h1>
+    <ul>
+      <li>
+        <ToDoItem label='My ToDo Item' :done='true'/>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import ToDoItem from './components/TodoItem';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ToDoItem,
+  },
 }
 </script>
 
@@ -22,7 +27,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <input type='checkbox' id='todo-item' :checked='isDone' />
+        <label :for='id'>{{label}}</label>
+    </div>
+</template>
+
+<script>
+import uniqueId from 'lodash.uniqueid';
+
+export default {
+    props: {
+        label: { required: true, type: String },
+        done: { default: false, type: Boolean },
+    },
+    data() {
+        return {
+            isDone: this.done,
+            id: uniqueId('todo-'),
+        };
+    },
+};
+</script>
